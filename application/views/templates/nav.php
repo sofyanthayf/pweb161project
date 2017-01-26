0b0722751161e6102cc64101c23bb883faac7d31
Sofyan Thayf <sofyanthayf@gmail.com>
2017-01-26 09:47:01 +0800

    Ganti Judul

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">SB Admin</a>
+        <a class="navbar-brand" href="index.html">PROJECT 161</a>
     </div>
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
